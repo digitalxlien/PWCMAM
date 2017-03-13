4fc6f2f893f9aed3b905630b4b89a65bf72dde9f
Brandon Villa <bornofos@gmail.com>
2017-03-12 20:03:56 -0600

    NavBar Added
    Se agregó la barra de navegación

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Colegio Militarizado Águilas de México</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">CMAM</a>
+    </div>
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Nuestra Institución <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Bienvenida</a></li>
+              <li><a href="#">Historia</a></li>
+              <li><a href="#">Educación Moral</a></li>
+              {{-- <li class="divider"></li> --}}
+              <li><a href="#">Organización</a></li>
+              <li><a href="#">Instalaciones</a></li>
+            </ul>
+          </li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Admisión <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Convocatoria y Solicitud</a></li>
+              <li><a href="#">Inscríbete</a></li>
+              <li><a href="#">Requisitos</a></li>
+            </ul>
+          </li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Académico <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Cuadro de Honor</a></li>
+              <li><a href="#">Preparatoria Militarizada</a></li>
+              <li><a href="#">Cursos</a></li>
+              <li><a href="#">Apuntes</a></li>
+              <li><a href="#">Egresados</a></li>
+            </ul>
+          </li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Vida del Cadete <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Galería de Facebook</a></li>
+              <li><a href="#">Campañas</a></li>
+              <li><a href="#">Maniobras</a></li>
+              <li><a href="#">Servicios</a></li>
+              <li><a href="#">Galería Fotográfica</a></li>
+            </ul>
+          </li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Deportes <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Fútbol</a></li>
+              <li><a href="#">Básquetbol</a></li>
+              <li><a href="#">Voleybol</a></li>
+              <li><a href="#">Equitación</a></li>
+              <li><a href="#">Senderismo y Montañismo</a></li>
+              <li><a href="#">Airsoft y Gotcha</a></li>
+              <li><a href="#">Natación y Buceo</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</body>
+</html>
