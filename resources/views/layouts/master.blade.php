9d71f3f23b7ee58e7a4b97e1ce4b739716656fda
Brandon Villa <bornofos@gmail.com>
2017-03-13 19:45:53 -0600

    Se agregó el footer
    Se creo el archivo footer.blade.php y de manda a llamar desde el master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,7 @@
     <title>Colegio Militarizado Águilas de México</title>
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/complements.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
@@ -12,7 +13,20 @@
 
   @include('layouts.nav')
 
-  @yield('content')
+  {{-- Contenedor principal --}}
+  <div class="container">
+    {{-- Fila --}}
+    <div class="row">
 
+      {{-- Columna --}}
+      <div class="col-md-6">
+        @yield('content')
+      </div>
+
+    </div>
+  </div>
+
+  @include('layouts.footer')
+  
 </body>
 </html>
