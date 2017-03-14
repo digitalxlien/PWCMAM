e71453eaa6c4528adf3da87062354023ee6206ba
Brandon Villa <bornofos@gmail.com>
2017-03-13 18:22:58 -0600

    Creación de nav, master
    Se obtiene el siguiente directorio:
    -views
    ---layouts
    -------master.blade.php
    -------nav.blade.php
    ---posts
    -------index.blade.php
    
    En nav.blade.php:
    Va todo el html para la barra de navegacion
    
    En master.blade.php:
    Contiene el cuerpo HTML principal, se trae el archivo nav.blade.php y el
    contenido de index.blade.php

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Colegio Militarizado Águilas de México</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+  @include('layouts.nav')
+
+  @yield('content')
+
+</body>
+</html>
