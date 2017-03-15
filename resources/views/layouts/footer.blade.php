c8de1c3bc5d397d66d52c94c4da63c7eb32e50d9
digitalx <digitalxlien@gmail.com>
2017-03-15 16:32:29 -0600

    Footer Edited
    Se corrigió el ajuste del footer con motivos de corrección responsive y
    que quedara centrado horizontalmente.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,66 +1,64 @@
 <footer>
   <div class="footer" id="footer">
-      <div class="container">
-          <div class="row">
-              <div class="col-md-2">
-                  <h3> Nuestra Institución </h3>
-                  <ul>
-                    <li><a href="#">Bienvenida</a></li>
-                    <li><a href="#">Historia</a></li>
-                    <li><a href="#">Educación Moral</a></li>
-                    <li><a href="#">Organización</a></li>
-                    <li><a href="#">Instalaciones</a></li>
-                  </ul>
-                </div>
-                <div class="col-md-2">
-                  <h3> Admisión </h3>
-                  <ul>
-                    <li><a href="#">Convocatoria y Solicitud</a></li>
-                    <li><a href="#">Inscríbete</a></li>
-                    <li><a href="#">Requisitos</a></li>
-                  </ul>
-                </div>
-                <div class="col-md-2">
-                  <h3> Académico </h3>
-                  <ul>
-                    <li><a href="#">Cuadro de Honor</a></li>
-                    <li><a href="#">Preparatoria Militarizada</a></li>
-                    <li><a href="#">Cursos</a></li>
-                    <li><a href="#">Apuntes</a></li>
-                    <li><a href="#">Egresados</a></li>
-                  </ul>
-                </div>
-                <div class="col-md-2">
-                  <h3> Vida del Cadete </h3>
-                  <ul>
-                    <li><a href="#">Galería de Facebook</a></li>
-                    <li><a href="#">Campañas</a></li>
-                    <li><a href="#">Maniobras</a></li>
-                    <li><a href="#">Servicios</a></li>
-                    <li><a href="#">Galería Fotográfica</a></li>
-                  </ul>
-                </div>
-                <div class="col-md-2">
-                  <h3> Deportes </h3>
-                  <ul>
-                    <li><a href="#">Fútbol</a></li>
-                    <li><a href="#">Básquetbol</a></li>
-                    <li><a href="#">Voleybol</a></li>
-                    <li><a href="#">Equitación</a></li>
-                    <li><a href="#">Senderismo y Montañismo</a></li>
-                    <li><a href="#">Airsoft y Gotcha</a></li>
-                    <li><a href="#">Natación y Buceo</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-    </div>
-    <!--/.footer-->
-
-    <div class="footer-bottom">
-        <div class="center">
-            <p class="pull-left"> Copyright 2017 ©. All right reserved. </p>
+        <div class="col-md-2">
+          <h3> Nuestra Institución </h3>
+          <ul>
+            <li><a href="#">Bienvenida</a></li>
+            <li><a href="#">Historia</a></li>
+            <li><a href="#">Educación Moral</a></li>
+            <li><a href="#">Organización</a></li>
+            <li><a href="#">Instalaciones</a></li>
+          </ul>
         </div>
+        <div class="col-md-2">
+          <h3> Admisión </h3>
+          <ul>
+            <li><a href="#">Convocatoria y Solicitud</a></li>
+            <li><a href="#">Inscríbete</a></li>
+            <li><a href="#">Requisitos</a></li>
+          </ul>
+        </div>
+        <div class="col-md-2">
+          <h3> Académico </h3>
+          <ul>
+            <li><a href="#">Cuadro de Honor</a></li>
+            <li><a href="#">Preparatoria Militarizada</a></li>
+            <li><a href="#">Cursos</a></li>
+            <li><a href="#">Apuntes</a></li>
+            <li><a href="#">Egresados</a></li>
+          </ul>
+        </div>
+        <div class="col-md-2">
+          <h3> Vida del Cadete </h3>
+          <ul>
+            <li><a href="#">Galería de Facebook</a></li>
+            <li><a href="#">Campañas</a></li>
+            <li><a href="#">Maniobras</a></li>
+            <li><a href="#">Servicios</a></li>
+            <li><a href="#">Galería Fotográfica</a></li>
+          </ul>
+        </div>
+        <div class="col-md-2">
+          <h3> Deportes </h3>
+          <ul>
+            <li><a href="#">Fútbol</a></li>
+            <li><a href="#">Básquetbol</a></li>
+            <li><a href="#">Voleybol</a></li>
+            <li><a href="#">Equitación</a></li>
+            <li><a href="#">Senderismo y Montañismo</a></li>
+            <li><a href="#">Airsoft y Gotcha</a></li>
+            <li><a href="#">Natación y Buceo</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
-    <!--/.footer-bottom-->
+  </div>
+  <!--/.footer-->
+
+  <div class="footer-bottom">
+    <div class="center">
+      <p class="pull-left"> Copyright 2017 ©. All right reserved. </p>
+    </div>
+  </div>
+  <!--/.footer-bottom-->
 </footer>
