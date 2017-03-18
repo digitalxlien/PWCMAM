eb4f809612b00dc4e64c68557ada026045b18975
Brandon Villa <bornofos@gmail.com>
2017-03-18 17:41:25 -0600

    Se creo el login
    Se crea el controlador para la sesion, la ruta y la vistta

---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="col-lg-12 pull-center">
+  <div class=" col-lg-6 col-md-6 col-sm-8 col-xs-8">
+
+    <form class="form-group" action="" method="POST">
+      {{ csrf_field() }}
+
+      <fieldset>
+        <legend>Iniciar Sesión</legend>
+
+        <div class="form-group">
+          <label for="email" class="label-title">Correo electrónico:</label>
+          <input type="email" class="form-control" name="email" id="email" required>
+        </div>
+
+        <div class="form-group">
+          <label for="email" class="label-title">Contraseña:</label>
+          <input type="password" class="form-control" name="password" id="pasword" required>
+        </div>
+
+        <div class="form-group">
+          <button type="submit" name="button" value="send" class="btn btn-default btn-group-justified">Aceptar</button>
+        </div>
+
+      </fieldset>
+    </form>
+  </div>
+</div>
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,6 @@ Route::get('/', 'PostsController@index');
 Route::get('/bienvenida', function() {
   return view('institucion.bienvenida');
 });
+
+Route::get('/login', 'SessionsController@create');
+Route::post('/login', 'SessionsController@store');
