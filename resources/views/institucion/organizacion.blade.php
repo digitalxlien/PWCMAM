858b7eb7b79c1933192d0dda548bd0bae00b62ad
Brandon Villa <bornofos@gmail.com>
2017-03-20 11:05:55 -0600

    Organizacion view added

---
+++ resources/views/institucion/organizacion.blade.php
@@ -0,0 +1,155 @@
+@extends('layouts.master')
+
+@section('content')
+  <div class="col-lg-8 col-md-8 col-sm-12">
+
+    <div>
+      <h3 class="text-center"><a href="#">Reglamento General del Colegio Militarizado "Aguilas de México"</a></h3>
+    </div>
+    <div class="jumbotron text-center">
+      <h4><b>Comentarios al correo:</b></h4>
+      <a href="mailto:cmam@aguilas.org.mx?subject=Comentarios_desde_pagina_web">cmam@aguilas.org.mx</a>
+    </div>
+    <div >
+      <p class="text-justify">
+        El Colegio Militarizado Aguilas de México esta conformado como Asociación Civil,  siendo constituida en Ciudad Guzmán, Jalisco mediante escritura pública No. 9608 con fecha del 1o. de Abril de 1995 ante el notario público No. 5 de esa localidad
+      </p>
+      <hr>
+    </div>
+     <div class="jumbotron">
+      <h3 class="text-center text-primary">ASAMBLEA DE ASOCIADOS</h3>
+
+      <h4 class="text-center text-primary list-group-item active">ASOCIADOS FUNDADORES:</h4>
+      <table class="table table-hover table-responsive">
+        <tbody>
+          <tr>
+            <td class="text-left  font-small">2/o.  Jefe;</td>
+            <td class="text-right font-small">Dr. Cs. Educ.	Gonzalo Asael Benavides Romero</td>
+          </tr>
+          <tr>
+            <td class="text-left font-small">Oficial</td>
+            <td class="text-right font-small">Hector Preciado Rodríguez</td>
+          </tr>
+          <tr>
+            <td class="text-left font-small">Oficial</td>
+            <td class="text-right font-small">Luis Manuel Díaz Sánchez</td>
+          </tr>
+          <tr>
+            <td class="text-left font-small">Arquitecto</td>
+            <td class="text-right font-small">Gerardo Aarón Benavides Romero</td>
+          </tr>
+          <tr>
+            <td class="text-left font-small">C.P.</td>
+            <td class="text-right font-small">Gonzalo Benavides Zamora (+)</td>
+          </tr>
+          <tr>
+            <td class="text-left font-small">Oficial</td>
+            <td class="text-right font-small">Cesar Usiel Morán Vázquez</td>
+          </tr>
+          <tr>
+            <td class="text-left font-small">Mayor M.C. Ret. </td>
+            <td class="text-right font-small">Amador Romero Lara (+)</td>
+          </tr>
+
+        </tbody>
+      </table>
+     </div>
+     <hr>
+    <div class="jumbotron" >
+      <h3 class="text-center text-primary">CONSEJO DIRECTIVO 2012 - 2016</h3>
+
+      <h4 class="text-center text-primary list-group-item active">PRESIDENTE FUNDADOR</h4>
+      <table class="table table-hover table-responsive">
+        <tbody>
+          <tr>
+            <td class="text-left  font-small">2/o.  Jefe;</td>
+            <td class="text-right font-small">Dr. Cs. Educ.	Gonzalo Asael Benavides Romero</td>
+          </tr>
+        </tbody>
+      </table>
+      <h4 class="text-center text-primary list-group-item active">SECRETARIO</h4>
+      <table class="table table-hover table-responsive">
+        <tbody>
+          <tr>
+            <td class="text-left  font-small">Oficial </td>
+            <td class="text-right font-small">Cesar Usiel Morán Vázquez</td>
+          </tr>
+        </tbody>
+      </table>
+      <h4 class="text-center text-primary list-group-item active">TESORERO</h4>
+      <table class="table table-hover table-responsive">
+        <tbody>
+          <tr>
+            <td class="text-left  font-small">Oficial </td>
+            <td class="text-right font-small">Luis Manuel Díaz Sánchez</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <hr>
+    <div class="jumbotron">
+     <h3 class="text-center text-primary">DIRECCION GENERAL</h3>
+
+     <h4 class="text-center text-primary list-group-item active">DIRECTOR GENERAL DEL C.M.A.M.</h4>
+     <table class="table table-hover table-responsive">
+       <tbody>
+         <tr>
+           <td class="text-left  font-small">2/o.  Jefe;</td>
+           <td class="text-center font-small">Dr. Cs. Educ.	Gonzalo Asael Benavides Romero</td>
+           <td class="text-right font-small">gbenavides@aguilas.org.mx </td>
+         </tr>
+       </tbody>
+     </table>
+     <h4 class="text-center text-primary list-group-item active">ASESOR MILITAR</h4>
+     <table class="table table-hover table-responsive">
+       <tbody>
+         <tr>
+           <td class="text-left  font-small">Capitán 1/o. C. D. </td>
+           <td class="text-right font-small">Carlos Enrique Flores Hidalgo</td>
+         </tr>
+       </tbody>
+     </table>
+   </div>
+   <hr>
+   <div class="jumbotron" >
+     <h3 class="text-center text-primary">JEFATURAS GENERALES</h3>
+
+     <h4 class="text-center text-primary list-group-item active">COMANDANTE DEL CUERPO DE CADETES</h4>
+     <table class="table table-hover table-responsive">
+       <tbody>
+         <tr>
+           <td class="text-left  font-small">Mayor F.A.P.A. D.E.M.A.</td>
+           <td class="text-right font-small">HERIBERTO SERNA MADERO</td>
+         </tr>
+       </tbody>
+     </table>
+     <h4 class="text-center text-primary list-group-item active">JEFE SECC. ACADEMICA</h4>
+     <table class="table table-hover table-responsive">
+       <tbody>
+         <tr>
+           <td class="text-left  font-small">Oficial, Licenciada en Psicología</td>
+           <td class="text-right font-small">TERESA DE JESUS MAGAÑA ALCARAZ </td>
+         </tr>
+       </tbody>
+     </table>
+     <h4 class="text-center text-primary list-group-item active">JEFE SECC. PEDAGOGICA </h4>
+     <table class="table table-hover table-responsive">
+       <tbody>
+         <tr>
+           <td class="text-left  font-small">Oficial, Licenciada en Psicología</td>
+           <td class="text-right font-small">MARIA EUGENIA PADILLA VELARDE</td>
+         </tr>
+       </tbody>
+     </table>
+     <h4 class="text-center text-primary list-group-item active">JEFATURA SECC. ADMINISTRATIVA</h4>
+     <table class="table table-hover table-responsive">
+       <tbody>
+         <tr>
+           <td class="text-left  font-small">Comandante de Escuadra</td>
+           <td class="text-right font-small">Mayra Guadalupe Cortés Gutiérrez</td>
+         </tr>
+       </tbody>
+     </table>
+   </div>
+  </div>
+@endsection
