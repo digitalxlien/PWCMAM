ddbc14a5d9e60ebc7e197679942c9a8865b794db
Martín Pacheco <digitalxlien@gmail.com>
2017-03-20 01:24:35 -0600

    Favicon Added and corrections
    It was added the main favicon.ico, this icon image appears in the
    explorer tab as a representative image of site.
    Corrections on campanas.blade.php

--- resources/views/vida-del-cadete/campanas.blade.php
+++ resources/views/vida-del-cadete/campanas.blade.php
@@ -2,52 +2,64 @@
 
 @section('content')
 <div class="col-lg-8 col-md-8 col-sm-12">
-  <div class="col-lg-12 jumbotron">
-    <h1>Campañas</h1>
+  <!--div class="col-lg-12 jumbotron">-->
+
+  <h1 class="text-center text-primary">Campañas</h1>
+  <hr>
+  <h1></h1>
 
 
-    <p class="text-justify">
-      El Colegio Militarizado "Aguilas de México", se encuentra siempre dispuesto a ayudar en la
-      medida de sus recursos a la Sociedad a la que pertenece, por tal motivo se tienen contempladas
-      diferentes actividades para apoyar el bienestar comunitario, entre estas acciones se encuentran:
-    </p>
+  <p class="text-justify font-medium">
+    El Colegio Militarizado Águilas de México, se encuentra siempre dispuesto a ayudar en la
+    medida de sus recursos a la Sociedad a la que pertenece, por tal motivo se tienen contempladas
+    diferentes actividades para apoyar el bienestar comunitario, entre estas acciones se encuentran:
+  </p>
 
-    <h2> Auxilios en casos de desastres </h2>
-    <img class="col-md-12 img-fluid" src="images/image-md.jpg">
-    <small class="text-justify">
+  <ul>
+    <li>Auxilios en casos de desastres.</li>
+    <li>Campañas de reforestación.</li>
+    <li>Combate de incendios forestales.</li>
+    <li>Auxilio vial al turista.</li>
+    <li>Rescate y salvamento.</li>
+    <li>Vallas de seguridad.</li>
+    <li>Operativo de apoyo a los visitantes en el Nevado de Colima.</li>
+  </ul>
+  <br>
+  <img class="img-fluid" src="images/image-md.jpg">
+  <p class="text-justify">
+    <blockquote class="font-small">
       Personal del CMAM implementando el "Plan Operativo en Desastres" en Cihuatlán, Jalisco.
       Este plan marca el procedimiento de operar en casos de desastre, para que nuestro apoyo
       sea oportuno, eficiente y eficáz. teniendo programadas actividades en caso de incendios,
       accidentes carreteros, una posible erupción del Volcán Colima, inundaciones, derrumbes, entre otras.
-    </small>
+    </blockquote>
+  </p>
 
-    <img src="images/image-md.jpg" class=" img-fluid">
-    <p class="text-justify">
+  <img class="img-fluid" src="images/image-md.jpg">
+  <p class="text-justify">
+    <blockquote class="font-small">
       Personal realizando prácticas de Rescate y Salvamento, cruzando una barranca con la técnica de
-       "Tirolesa", esta técnica permite hacer llegar los servicios de emergencia hasta zonas que han
-       sido debastadas por la naturaleza, como derrumbe de puentes, etc.
-    </p>
+      "Tirolesa", esta técnica permite hacer llegar los servicios de emergencia hasta zonas que han
+      sido debastadas por la naturaleza, como derrumbe de puentes, etc.
+    </blockquote>
+  </p>
 
-    <img class="col-lg-12 img-fluid" src="images/image-md.jpg">
-    <p class="text-justify">
+  <img class="img-fluid" src="images/image-md.jpg">
+  <p class="text-justify">
+    <blockquote class="font-small">
       En esta imagen observamos al grupo Táctico, realizar un descenso de la catedral de Cd. Guzmán,
       con la técnica de Teleférico, en ocasiones útil para llegar de manera rápida y segura a lugares
       agrestes, así como para descender o ascender lesionados en camilla.
-    </p>
+    </blockquote>
+  </p>
 
-    <img class="col-lg-12 img-fluid" src="images/image-md.jpg">
-    <p class="text-justify">
+  <img class="img-fluid" src="images/image-md.jpg">
+  <p class="text-justify">
+    <blockquote class="font-small">
       Exhibición de Tombling, donde el personal cruza la "Cama de Fuego"
       con un salto del tigre por arriba y tambien a rastras. ¡Un error puede ser fatal!
-    </p>
-
-  </div>
-
-  <div class="col-lg- jumbotron">
-    <p class="text-justify">Es una institución que fué fundada en 1987 como asociación civil y con 29 años de trayectoria institucional y 10 años de experiencia continua al lograr ofrecer un modelo educativo enfocado al desarrollo pleno del alumnado, logrando infundir en nuestros egresados una semilla de liderazgo social en beneficio de nuestra nación.</p>
-    <p class="text-justify">Hemos observado con satisfacción como muchos de nuestros egresados han decidido continuar con la carrera de las armas integrándose a los diversos planteles Militares del Ejército, Fuerza Aérea o Marina, así también hemos observado a otros jóvenes que como profesionistas han aplicado los principios que rigen a nuestra institución y han destacado en sus áreas de desempeño,tanto en sus estudios como en su trabajo. </p>
-  </div>
-
+    </blockquote>
+  </p>
 </div>
 
 @endsection
