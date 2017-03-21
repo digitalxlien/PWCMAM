4a62fd7a571d2a43ffa43e34b46706a88e9eaef2
marcos8896 <elyomarcos@gmail.com>
2017-03-20 19:14:15 -0600

    preparatoria view added

---
+++ resources/views/academico/preparatoria.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.master')
+
+@section('content')
+  <div class="col-lg-8 col-md-8 col-sm-12">
+    <div>
+      <h2 class="text-center text-primary">EDUCACIÓN MEDIA SUPERIOR</h2>
+    </div><hr>
+    <blockquote class="text-justify jumbotron">
+      El Colegio Militarizado "Aguilas de México" te ofrece la opción para cursar tu preparatoria, con un sistema de enseñanza integral, bajo una estricta disciplina y con una educación de excelencia, garantizando con ello tu proceso de enseñanza-aprendizaje.
+    </blockquote>
+
+    <blockquote>
+      <h3 class="text-center text-primary list-group-item list-group-item-warning">¡INSCRIPCIONES ABIERTAS!</h3>
+    </blockquote>
+
+     <div class="jumbotron">
+      <h4 class="text-center text-primary list-group-item active">VENTAJAS DE ESTUDIAR CON NOSOTROS:</h4>
+      <table class="table table-hover table-responsive">
+        <tbody>
+          <tr>
+            <td class="text-justify font-small">Acondicionamiento físico y prácticas deportivas diariamente.</td>
+          </tr>
+          <tr>
+            <td class="text-justify font-small">Regimén alimenticio balanceado: con las proteínas, vitaminas y complementos necesarios para su correcta cormación y desarrollo.</td>
+          </tr>
+          <tr>
+            <td class="text-justify font-small">Desarrollo de un carácter sólido, basado en principios y altos códigos de conducta.</td>
+          </tr>
+          <tr>
+            <td class="text-justify font-small">Formación de buenos hábitos en su descanso, alimentación y estudio.</td>
+          </tr>
+          <tr>
+            <td class="text-justify font-small">Enseñar a los alumnos a valerse por sí mismos en su limpieza, orden, arreglo de vestuario, tareas y demás actividades cotidianas.</td>
+          </tr>
+          <tr>
+            <td class="text-justify font-small">Servicio de enfermería, peluquería, lavandería, cafetería, entre otros.</td>
+          </tr>
+          <tr>
+            <td class="text-justify font-small">Constantes visitas de estudio a planteles militares, universidades, museos, zonas arquelógicas, campamentos, etc. </td>
+          </tr>
+          <tr>
+            <td class="text-justify font-small">Fomento de la sana competencia académica y deportiva con otras instituciones de nivel medio superior.</td>
+          </tr>
+          <tr>
+            <td class="text-justify font-small">Al terminar tus estudios podrás ingresar a cualquier institución de tipo superior, publica o privada.</td>
+          </tr>
+          <tr>
+            <td class="text-justify font-small">Acceso a los apoyos bibliográficos necesarios para asegurar la calidad de la educación que recibes.</td>
+          </tr>
+          <tr>
+            <td class="text-justify font-small">El <a href="#">plan de estudio</a> es elaborado por la S.E.P. bajo la modalidad de Bachillerato General por competencias ofreciendose  dos áreas de especialidad: <b>Administración</b> e <b>Informática</b> </td>
+          </tr>
+        </tbody>
+      </table>
+     </div>
+     <hr>
+
+  </div>
+@endsection
