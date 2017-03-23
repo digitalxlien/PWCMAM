2bcecce910122ba4d4f46032ec43bdde9fbdddee
marcos8896 <elyomarcos@gmail.com>
2017-03-22 19:57:19 -0600

    "apuntes" view added

---
+++ resources/views/academico/apuntes.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.master')
+
+@section('content')
+  <div class="col-lg-8 col-md-8 col-sm-12">
+    <div>
+      <h2 class="text-center text-primary">APUNTES DE INSTRUCCION</h2>
+    </div><hr>
+
+    <div class="jumbotron">
+      <h4 class="text-center text-primary list-group-item active">APUNTES DE INSTRUCCION:</h4>
+      <table class="table table-hover table-responsive table-bordered">
+        <tbody>
+          <tr>
+            <td class="text-justify font-small"><a href="#">01-LM-02 LEGISLACION MILITAR</a></td>
+          </tr>
+          <tr>
+            <td class="text-justify font-small"><a href="#">01-PA-02 PRIMEROS AUXILIOS</a></td>
+          </tr>
+          <tr>
+            <td class="text-justify font-small"><a href="#">01-RC-02 RADIO-COMUNICACIONES</a></td>
+          </tr>
+          <tr>
+            <td class="text-justify font-small"><a href="#">01-LC-02 LECTURA DE CARTAS TOPOGRAFICAS</a></td>
+          </tr>
+          <tr>
+            <td class="text-justify font-small"><a href="#">01-RS-02 RESCATE Y SALVAMENTO</a></td>
+          </tr>
+          <tr>
+            <td class="text-justify font-small"><a href="#">01-SB-02 SUPERVIVENCIA</a></td>
+          </tr>
+        </tbody>
+      </table>
+     </div>
+
+  </div>
+@endsection
