c99a9db4b34959c14d4edee885742d64fd048f8e
marcos8896 <elyomarcos@gmail.com>
2017-03-23 19:23:24 -0600

    Tiny change for "egresados" view.

--- resources/views/academico/egresados.blade.php
+++ resources/views/academico/egresados.blade.php
@@ -6,7 +6,7 @@
       <h2 class="text-center text-primary">FOTOGRAFIAS DEL PERSONAL EGRESADO</h2>
     </div><hr>
     <div class="jumbotron">
-      <h3 class="text-center text-primary">BACHILLERATO:</h3>
+      <h3 class="text-center text-primary list-group-item active">BACHILLERATO</h3><hr>
       <table class="table table-hover table-responsive table-bordered">
         <tbody>
           <tr>
@@ -18,72 +18,75 @@
         </tbody>
       </table>
 
-      <table class="table table-hover table-responsive table-bordered">
-        <tbody>
-          <tr>
-            <td><img src="images/egresados/02.jpg" class="img-rounded img-responsive"></td>
-          </tr>
-          <tr>
-            <td><h4 class="text-center text-primary">Generación 2005-2006 (1/er. Sector)</h4></td>
-          </tr>
-        </tbody>
-      </table>
+     </div>
 
-      <table class="table table-hover table-responsive table-bordered">
-        <tbody>
-          <tr>
-            <td><img src="images/egresados/03.jpg" class="img-rounded img-responsive"></td>
-          </tr>
-          <tr>
-            <td><h4 class="text-center text-primary">Generación 2000-2001 (2/o. Sector)</h4></td>
-          </tr>
-        </tbody>
-      </table>
+     <div class="jumbotron">
+       <h3 class="text-center text-primary list-group-item active">ADIESTRAMIENTO MILITARIZADO</h3><hr>
+       <table class="table table-hover table-responsive table-bordered">
+         <tbody>
+           <tr>
+             <td><img src="images/egresados/02.jpg" class="img-rounded img-responsive"></td>
+           </tr>
+           <tr>
+             <td><h4 class="text-center text-primary">Generación 2005-2006 (1/er. Sector)</h4></td>
+           </tr>
+         </tbody>
+       </table>
 
-      <table class="table table-hover table-responsive table-bordered">
-        <tbody>
-          <tr>
-            <td><img src="images/egresados/04.jpg" class="img-rounded img-responsive"></td>
-          </tr>
-          <tr>
-            <td><h4 class="text-center text-primary">Generación 1998-1999</h4></td>
-          </tr>
-        </tbody>
-      </table>
+       <table class="table table-hover table-responsive table-bordered">
+         <tbody>
+           <tr>
+             <td><img src="images/egresados/03.jpg" class="img-rounded img-responsive"></td>
+           </tr>
+           <tr>
+             <td><h4 class="text-center text-primary">Generación 2000-2001 (2/o. Sector)</h4></td>
+           </tr>
+         </tbody>
+       </table>
 
-      <table class="table table-hover table-responsive table-bordered">
-        <tbody>
-          <tr>
-            <td><img src="images/egresados/05.jpg" class="img-rounded img-responsive"></td>
-          </tr>
-          <tr>
-            <td><h4 class="text-center text-primary">Generación 1995-1996</h4></td>
-          </tr>
-        </tbody>
-      </table>
+       <table class="table table-hover table-responsive table-bordered">
+         <tbody>
+           <tr>
+             <td><img src="images/egresados/04.jpg" class="img-rounded img-responsive"></td>
+           </tr>
+           <tr>
+             <td><h4 class="text-center text-primary">Generación 1998-1999</h4></td>
+           </tr>
+         </tbody>
+       </table>
 
-      <table class="table table-hover table-responsive table-bordered">
-        <tbody>
-          <tr>
-            <td><img src="images/egresados/06.jpg" class="img-rounded img-responsive"></td>
-          </tr>
-          <tr>
-            <td><h4 class="text-center text-primary">Generación 1994-1995</h4></td>
-          </tr>
-        </tbody>
-      </table>
+       <table class="table table-hover table-responsive table-bordered">
+         <tbody>
+           <tr>
+             <td><img src="images/egresados/05.jpg" class="img-rounded img-responsive"></td>
+           </tr>
+           <tr>
+             <td><h4 class="text-center text-primary">Generación 1995-1996</h4></td>
+           </tr>
+         </tbody>
+       </table>
 
-      <table class="table table-hover table-responsive table-bordered">
-        <tbody>
-          <tr>
-            <td><img src="images/egresados/07.jpg" class="img-rounded img-responsive center"></td>
-          </tr>
-          <tr>
-            <td><h4 class="text-center text-primary">Generación 1993-1994</h4></td>
-          </tr>
-        </tbody>
-      </table>
+       <table class="table table-hover table-responsive table-bordered">
+         <tbody>
+           <tr>
+             <td><img src="images/egresados/06.jpg" class="img-rounded img-responsive"></td>
+           </tr>
+           <tr>
+             <td><h4 class="text-center text-primary">Generación 1994-1995</h4></td>
+           </tr>
+         </tbody>
+       </table>
 
+       <table class="table table-hover table-responsive table-bordered">
+         <tbody>
+           <tr>
+             <td><img src="images/egresados/07.jpg" class="img-rounded img-responsive center"></td>
+           </tr>
+           <tr>
+             <td><h4 class="text-center text-primary">Generación 1993-1994</h4></td>
+           </tr>
+         </tbody>
+       </table>
      </div>
 
 
