7869e0ac79c897d35a27ed9b5fcc0815cb57a406
Brandon Villa <bornofos@gmail.com>
2017-03-29 22:23:41 -0600

    Some changes in RegistrationController

--- app/Http/Controllers/RegistrationsController.php
+++ app/Http/Controllers/RegistrationsController.php
@@ -28,22 +28,22 @@ class RegistrationsController extends Controller
       'address'                 => request('address'),
       'neighborhood'            => request('neighborhood'),
       'city'                    => request('city'),
-      'state'                    => request('state'),
-      'country'                    => request('country'),
+      'state'                   => request('state'),
+      'country'                 => request('country'),
       'telephone'               => request('telephone'),
       'email'                   => request('email'),
       'date_birth'              => request('date_birth'),
       'height'                  => request('height'),
       'sex'                     => request('sex'),
       'minimal_schooling'       => request('minimal_schooling'),
-      'speciality'             => request('speciality'),
+      'speciality'              => request('speciality'),
       'current_occupation'      => request('current_occupation'),
       'inscription_request'     => request('inscription_request'),
       'willing_to_live_jalisco' => request('willing_to_live_jalisco'),
       'comments'                => request('comments'),
       'questions'               => request('questions'),
       ]);
-
+      //redirecciona al home
       return redirect('/');
 
 
